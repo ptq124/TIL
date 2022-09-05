ab90fa9ba1712aef7ba381d8151b92a11bbf4688
ptq124 <64758823+ptq124@users.noreply.github.com>
2022-09-05 11:35:35 +0900

    추가 okr작성법

--- docs/.vuepress/const.js
+++ docs/.vuepress/const.js
@@ -1,12 +1,11 @@
 exports.JavaScriptList = [
-    "JavaScript/builtin-object.md",
-    "JavaScript/string.md",
-    "JavaScript/array.md",
+  "JavaScript/builtin-object.md",
+  "JavaScript/string.md",
+  "JavaScript/array.md",
 ];
-exports.VuejsList = [
-    "Vue/hellovue.md",
-]
+exports.VuejsList = ["Vue/hellovue.md"];
 exports.VuePressList = [
-    "vuepress/vuepress-start.md",
-    "vuepress/vuepress-deploy.md",
+  "vuepress/vuepress-start.md",
+  "vuepress/vuepress-deploy.md",
 ];
+exports.etc = ["etc/okr.md"];
