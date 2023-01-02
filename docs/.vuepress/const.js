09152e4e97dbb6066025b875d915a41ae100e70a
박태규 Park Tae Kyu <ptq124@naver.com>
2023-01-02 22:09:33 +0900

    Update const.js

--- docs/.vuepress/const.js
+++ docs/.vuepress/const.js
@@ -24,10 +24,10 @@ exports.JavaScriptList = [
   "JavaScript/arrow.md",
 ]
 exports.VuejsList = ["Vue/hellovue.md", "Vue/vue-sheetjs.md"]
-exports.FrontendList = ["frontend/bem.md"]
 exports.FastApiList = ["fastapi/crawl.md", "fastapi/db.md"]
 exports.VuePressList = [
   "vuepress/vuepress-start.md",
   "vuepress/vuepress-deploy.md",
 ]
 exports.etc = ["etc/okr.md"]
+exports.FrontendList = ["frontend/bem.md"]
